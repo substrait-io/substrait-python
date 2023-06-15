fdf68eafa945994e55fe1782adb302b4443e7282
Gil Forsyth <gforsyth@users.noreply.github.com>
2023-06-15 14:41:49 -0400

    feat: add upstream substrait version to package (#14)

--- src/substrait/__init__.py
+++ src/substrait/__init__.py
@@ -2,3 +2,7 @@ try:
     from ._version import __version__
 except ImportError:
     pass
+
+__substrait_version__ = "0.30.0"
+__substrait_hash__ = "3259a1ba"
+__minimum_substrait_version__ = "0.30.0"

aad096b1befaf5bc9cde174c1f53424b3b8a958a
Gil Forsyth <gil@forsyth.dev>
2023-06-15 10:08:32 -0400

    feat: package extension yamls

--- gen_proto.sh
+++ gen_proto.sh
@@ -7,6 +7,7 @@ submodule_dir=./third_party/substrait
 src_dir="$submodule_dir"/proto
 tmp_dir=./buf_work_dir
 dest_dir=./src/substrait/gen
+extension_dir=./src/substrait/extensions
 
 # Prefix the protobuf files with a unique configuration to prevent namespace conflicts
 # with other substrait packages. Save output to the work dir.
@@ -18,6 +19,13 @@ rm -rf "$dest_dir"
 # Generate the new python protobuf files
 buf generate
 protol --in-place --create-package --python-out "$dest_dir" buf
+
+# Remove the old extension files
+rm -rf "$extension_dir"
+
+# Copy over new yaml files
+cp -fr "$submodule_dir"/extensions "$extension_dir"
+find "$extension_dir" -type f -exec chmod u+rw {} +
 
 # Remove the temporary work dir
 rm -rf "$tmp_dir"

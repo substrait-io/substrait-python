348a7b76f5feb77e798d3afeeee18bead467c4b6
Gil Forsyth <gforsyth@users.noreply.github.com>
2023-09-27 17:38:32 -0400

    chore: upgrade substrait to v0.34.0 (#23)

--- src/substrait/__init__.py
+++ src/substrait/__init__.py
@@ -3,6 +3,6 @@ try:
 except ImportError:
     pass
 
-__substrait_version__ = "0.32.0"
-__substrait_hash__ = "31b99906"
+__substrait_version__ = "0.34.0"
+__substrait_hash__ = "dacc5a9"
 __minimum_substrait_version__ = "0.30.0"

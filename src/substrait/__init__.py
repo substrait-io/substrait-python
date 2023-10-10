2cea66273b758ecca8dbc854717ab45227f3b9cb
Gil Forsyth <gil@forsyth.dev>
2023-10-10 09:22:38 -0400

    chore(substrait): bump to v0.36.0

--- src/substrait/__init__.py
+++ src/substrait/__init__.py
@@ -3,6 +3,6 @@ try:
 except ImportError:
     pass
 
-__substrait_version__ = "0.35.0"
-__substrait_hash__ = "9e39067"
+__substrait_version__ = "0.36.0"
+__substrait_hash__ = "92302d8"
 __minimum_substrait_version__ = "0.30.0"

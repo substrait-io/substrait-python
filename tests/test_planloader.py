b06464cfafa51df90c84f9b8dce45c65401271af
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-08 23:14:41 -0800

    Updated based on review.

--- tests/test_planloader.py
+++ tests/test_planloader.py
@@ -5,7 +5,5 @@ from substrait.planloader import planloader
 
 
 def test_main():
-    print(planloader.__file__)
-    print(dir(planloader))
     testplan = planloader.load_substrait_plan('tests/tpch-plan01.json')
     planloader.save_substrait_plan(testplan, 'myoutfile.splan', planloader.PlanFileFormat.TEXT.value)

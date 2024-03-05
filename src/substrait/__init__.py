3db7ccce38087bed2791430684246ff33a38992d
Gil Forsyth <gforsyth@users.noreply.github.com>
2024-03-05 15:51:50 -0500

    chore(substrait): bump to v0.44.0 (#48)

--- src/substrait/__init__.py
+++ src/substrait/__init__.py
@@ -3,6 +3,6 @@ try:
 except ImportError:
     pass
 
-__substrait_version__ = "0.43.0"
-__substrait_hash__ = "5e1948e"
+__substrait_version__ = "0.44.0"
+__substrait_hash__ = "2e12da1"
 __minimum_substrait_version__ = "0.30.0"

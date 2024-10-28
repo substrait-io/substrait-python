b78a614788b082dbc06a09d47cdbddf81e97469e
Tornike Gurgenidze <tokoko96@gmail.com>
2024-10-29 01:17:59 +0400

    feat: add derivation expression evaluator (#63)
    Adds support for the evaluation of extension function's return type
    expressions.
    It's meant to be used by producers to correctly infer return types.
    It uses antlr grammar (temporarily) copy-pasted from `substrait-java`
    w/o changes.

---
+++ src/substrait/derivation_expression.py
@@ -0,0 +1,102 @@
+from typing import Optional
+from antlr4 import InputStream, CommonTokenStream
+from substrait.gen.antlr.SubstraitTypeLexer import SubstraitTypeLexer
+from substrait.gen.antlr.SubstraitTypeParser import SubstraitTypeParser
+from substrait.gen.proto.type_pb2 import Type
+
+
+def _evaluate(x, values: dict):
+    if type(x) == SubstraitTypeParser.BinaryExprContext:
+        left = _evaluate(x.left, values)
+        right = _evaluate(x.right, values)
+
+        if x.op.text == "+":
+            return left + right
+        elif x.op.text == "-":
+            return left - right
+        elif x.op.text == "*":
+            return left * right
+        elif x.op.text == ">":
+            return left > right
+        elif x.op.text == ">=":
+            return left >= right
+        elif x.op.text == "<":
+            return left < right
+        elif x.op.text == "<=":
+            return left <= right
+        else:
+            raise Exception(f"Unknown binary op {x.op.text}")
+    elif type(x) == SubstraitTypeParser.LiteralNumberContext:
+        return int(x.number.text)
+    elif type(x) == SubstraitTypeParser.TypeParamContext:
+        return values[x.identifier.text]
+    elif type(x) == SubstraitTypeParser.NumericParameterNameContext:
+        return values[x.Identifier().symbol.text]
+    elif type(x) == SubstraitTypeParser.ParenExpressionContext:
+        return _evaluate(x.expr(), values)
+    elif type(x) == SubstraitTypeParser.FunctionCallContext:
+        exprs = [_evaluate(e, values) for e in x.expr()]
+        func = x.Identifier().symbol.text
+
+        if func == "min":
+            return min(*exprs)
+        elif func == "max":
+            return max(*exprs)
+        else:
+            raise Exception(f"Unknown function {func}")
+    elif type(x) == SubstraitTypeParser.TypeContext:
+        scalar_type = x.scalarType()
+        parametrized_type = x.parameterizedType()
+        if scalar_type:
+            if isinstance(scalar_type, SubstraitTypeParser.I8Context):
+                return Type(i8=Type.I8())
+            elif isinstance(scalar_type, SubstraitTypeParser.I16Context):
+                return Type(i16=Type.I16())
+            elif isinstance(scalar_type, SubstraitTypeParser.I32Context):
+                return Type(i32=Type.I32())
+            elif isinstance(scalar_type, SubstraitTypeParser.I64Context):
+                return Type(i64=Type.I64())
+            elif isinstance(scalar_type, SubstraitTypeParser.Fp32Context):
+                return Type(fp32=Type.FP32())
+            elif isinstance(scalar_type, SubstraitTypeParser.Fp64Context):
+                return Type(fp64=Type.FP64())
+            elif isinstance(scalar_type, SubstraitTypeParser.BooleanContext):
+                return Type(bool=Type.Boolean())
+            else:
+                raise Exception(f"Unknown scalar type {type(scalar_type)}")
+        elif parametrized_type:
+            if isinstance(parametrized_type, SubstraitTypeParser.DecimalContext):
+                precision = _evaluate(parametrized_type.precision, values)
+                scale = _evaluate(parametrized_type.scale, values)
+                return Type(decimal=Type.Decimal(precision=precision, scale=scale))
+            raise Exception(f"Unknown parametrized type {type(parametrized_type)}")
+        else:
+            raise Exception("either scalar_type or parametrized_type is required")
+    elif type(x) == SubstraitTypeParser.NumericExpressionContext:
+        return _evaluate(x.expr(), values)
+    elif type(x) == SubstraitTypeParser.TernaryContext:
+        ifExpr = _evaluate(x.ifExpr, values)
+        thenExpr = _evaluate(x.thenExpr, values)
+        elseExpr = _evaluate(x.elseExpr, values)
+
+        return thenExpr if ifExpr else elseExpr
+    elif type(x) == SubstraitTypeParser.MultilineDefinitionContext:
+        lines = zip(x.Identifier(), x.expr())
+
+        for i, e in lines:
+            identifier = i.symbol.text
+            expr_eval = _evaluate(e, values)
+            values[identifier] = expr_eval
+
+        return _evaluate(x.finalType, values)
+    elif type(x) == SubstraitTypeParser.TypeLiteralContext:
+        return _evaluate(x.type_(), values)
+    else:
+        raise Exception(f"Unknown token type {type(x)}")
+
+
+def evaluate(x: str, values: Optional[dict] = None):
+    lexer = SubstraitTypeLexer(InputStream(x))
+    stream = CommonTokenStream(lexer)
+    parser = SubstraitTypeParser(stream)
+    return _evaluate(parser.expr(), values)

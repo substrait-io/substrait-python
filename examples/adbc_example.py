a9a52df7c3f3e213f2ac0c106a6dd8c0e2bbe7e1
tokoko <togurgenidze@gmail.com>
2025-05-03 10:41:42 +0000

    chore: add adbc, duckdb examples

--- examples/adbc_example.py
+++ examples/adbc_example.py
@@ -1,8 +1,7 @@
 """
-Install pyarrow, adbc_driver_duckdb and duckdb before running this example
+Install pyarrow, adbc-driver-manager and duckdb before running this example
 This example  currently can be run only with duckdb<=1.1.3, later vesions of duckdb no longer support substrait in adbc
 """
-
 
 import adbc_driver_duckdb.dbapi
 import pyarrow

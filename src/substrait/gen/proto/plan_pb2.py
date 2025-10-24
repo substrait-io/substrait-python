b0c1a566cb4a38126d8549fa997fa91a35599b0c
tokoko <togurgenidze@gmail.com>
2025-10-24 13:11:29 +0000

    chore: regenerate with new environment

--- src/substrait/gen/proto/plan_pb2.py
+++ src/substrait/gen/proto/plan_pb2.py
@@ -1,8 +1,10 @@
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
+_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 29, 5, '', 'proto/plan.proto')
 _sym_db = _symbol_database.Default()
 from ..proto import algebra_pb2 as proto_dot_algebra__pb2
 from ..proto.extensions import extensions_pb2 as proto_dot_extensions_dot_extensions__pb2
@@ -11,10 +13,10 @@ DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10proto/plan.pro
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'proto.plan_pb2', _globals)
-if _descriptor._USE_C_DESCRIPTORS == False:
-    _globals['DESCRIPTOR']._options = None
+if not _descriptor._USE_C_DESCRIPTORS:
+    _globals['DESCRIPTOR']._loaded_options = None
     _globals['DESCRIPTOR']._serialized_options = b'\n\x0eio.proto.protoP\x01\xaa\x02\x0eProto.Protobuf'
-    _globals['_PLAN'].fields_by_name['extension_uris']._options = None
+    _globals['_PLAN'].fields_by_name['extension_uris']._loaded_options = None
     _globals['_PLAN'].fields_by_name['extension_uris']._serialized_options = b'\x18\x01'
     _globals['_PLANREL']._serialized_start = 101
     _globals['_PLANREL']._serialized_end = 192

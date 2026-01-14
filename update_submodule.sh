8259dc1e32ef3e1a83cdeed5554f201b895aecb9
Victor Barua <victor.barua@datadoghq.com>
2026-01-13 16:48:29 -0800

    build: remove version updating code

--- update_submodule.sh
+++ update_submodule.sh
@@ -12,10 +12,4 @@ git submodule update --remote third_party/substrait
 DIR=$(cd "$(dirname "$0")" && pwd)
 pushd "${DIR}"/third_party/substrait/ || exit
 git checkout "$VERSION"
-SUBSTRAIT_HASH=$(git rev-parse --short HEAD)
 popd || exit
-
-VERSION=${VERSION//v/}
-
-sed -i "s#__substrait_hash__.*#__substrait_hash__ = \"$SUBSTRAIT_HASH\"#g" src/substrait/__init__.py
-sed -i "s#__substrait_version__.*#__substrait_version__ = \"$VERSION\"#g" src/substrait/__init__.py
